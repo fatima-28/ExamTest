aded9d824cd7dc6b314776d67e1c1370c78592a8
MamedovVasif <24Q8KZ1@code.edu.az>
2022-06-29 17:56:02 +0400

    crud will start

---
+++ Peterna Back/Peterna/Peterna/Areas/AdminPanel/Controllers/ServicesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peterna.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
